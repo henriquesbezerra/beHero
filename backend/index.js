b94e4ae46eb86f8c39f168c5ddeaac46f2d90d3d
HenriRique <henriquesb92@gmail.com>
2020-03-24 19:42:43 -0300

    Configuraçao nodemon e inicio sobre banco de dados

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,57 @@ const express = require('express');
 
 const app = express();
 
+// Antes de todas as requisições, converter json em objeto javascript
+app.use(express.json());
 
-app.get('/', (request, response) =>{
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Métodos HTTP:
+  * 
+  * GET: Buscar uma informção no back-end
+  * POST: Criar uma informação no back-end
+  * PUT: Alterar uma informação no back-end
+  * DELETE: Deletar uma informação no back-end
+  */
+
+
+  /***
+   * Tipos de parâmetros:
+   * 
+   * Query Params: Parametros nomeados enviados na rota após "?" (FIltros, páginação)
+   * Route Params: Parâmetros utilizados para identificar recursos
+   * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+   */
+
+
+
+   /**
+    * SQL: Mysql, SQlite, PostgressSQL, Oracle, Microsoft SQL Server 
+    * NoSQL: MongoDB, CouchDB, etc.
+    */
+
+app.get('/:id', (request, response) =>{
+    const params = request.params;
+    const queries = request.query;
+    const body = request.body;
+    console.log(params, queries);
     return response.json({
         evento: 'Semana OmniStack 11.0',
         aluno: 'Henrique Silva'
     });
 });
+
+app.post('/', (request, response) =>{   
+    const body = request.body;
+    console.log(body);
+    return response.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Henrique Silva'
+    });
+});
+
 
 app.listen(3333);
